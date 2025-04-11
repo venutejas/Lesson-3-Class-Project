b6fd7e9996c3f58f2beb5a5af7b195d3e146d445
venutejas <vtchevuri@gmail.com>
2025-04-11 17:48:10 -0500

    Python project Lesson 3

---
+++ First.py
@@ -0,0 +1,25 @@
+a = 4
+print("type of a:", type(a))
+
+b = 24.6
+print("type of b:", type(b))
+
+c = "Hello World!"
+print("type of c:", type(c))
+
+d = False
+print("type of d:", type(d))
+
+print("After Type Casting...")
+b = str(b)
+print(b)
+print("type of b", type(b))
+a = float(a)
+print(a)
+print("type of a", type(a))
+c = bool(c)
+print(c)
+print("type of c", type(c))
+d = int(d)
+print(d)
+print("type of d", type(d))
---
+++ Second.py
@@ -0,0 +1,4 @@
+x = str(input("Enter your value:"))
+y = x[::-1]
+x = y
+print(x)
